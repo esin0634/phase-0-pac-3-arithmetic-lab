68e89a995525d657d7db9e0fb74ecfcf5ff410cf
Esin <nbusra.esin@gmail.com>
2023-03-31 12:33:35 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function subtract(a,b) {
+    return a-b
+}
+
+function multiply(a,b) {
+    return a*b
+}
+
+function divide(a,b) {
+    return a/b
+}
+function add(a,b) {
+    return a+b
+}
+
+function increment(n){
+    return n+1
+}
+function decrement(n){
+    return n-1
+}
+
+function makeInt(n , a=10){
+    return parseInt(n,a)
+}
+function preserveDecimal (n , a=10){
+    return parseFloat(n,a)
+}
